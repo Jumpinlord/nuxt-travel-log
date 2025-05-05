8ec86d92e962278df2cd35f7af853558c9988280
Remzi Mustafaev <45316748+Jumpinlord@users.noreply.github.com>
2025-05-05 13:24:28 +0300

    setup eslint / lint-staged (#48)

---
+++ eslint.config.mjs
@@ -0,0 +1,47 @@
+// @ts-check
+import antfu from '@antfu/eslint-config';
+
+import withNuxt from './.nuxt/eslint.config.mjs';
+
+export default withNuxt(antfu(
+  {
+    type: 'app',
+    vue: true,
+    typescript: true,
+    formatters: true,
+    stylistic: {
+      indent: 2,
+      semi: true,
+      quotes: undefined,
+    },
+    ignores: ['.pnpm-store/**'],
+  },
+  {
+    rules: {
+      'ts/no-redeclare': 'off',
+      'ts/consistent-type-definitions': ['error', 'type'],
+      'no-console': ['warn'],
+      'antfu/no-top-level-await': ['off'],
+      'node/prefer-global/process': ['off'],
+      'node/no-process-env': ['error'],
+      'perfectionist/sort-imports': [
+        'error',
+        {
+          tsconfigRootDir: '.',
+        },
+      ],
+      'unicorn/filename-case': [
+        'error',
+        {
+          cases: {
+            camelCase: true,
+            kebabCase: true,
+            snakeCase: true,
+            pascalCase: true,
+          },
+          ignore: ['README.md'],
+        },
+      ],
+    },
+  },
+));
