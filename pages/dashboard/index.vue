9cdf44ac9f3c0edf9f0365577429af9e2cd0ba66
Remzi Mustafaev <45316748+Jumpinlord@users.noreply.github.com>
2025-05-13 22:12:56 +0300

    add expandable sidebar  #21

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,5 +1,34 @@
+<script setup lang="ts">
+const isSidebarOpen = ref(true);
+
+onMounted(() => {
+  isSidebarOpen.value = localStorage.getItem('isSidebarOpen') === 'true';
+});
+
+function toggleSidebar() {
+  isSidebarOpen.value = !isSidebarOpen.value;
+  localStorage.setItem('isSidebarOpen', isSidebarOpen.value.toString());
+}
+</script>
+
 <template>
-  <div class="container mx-auto">
-    <h1>You are logged in!</h1>
+  <div class="container mx-auto flex-1 flex">
+    <div :class="isSidebarOpen ? 'w-64' : 'w-16' " class="bg-base-100 transition-all duration-300">
+      <div
+        class="flex p-2 hover:cursor-pointer hover:bg-base-200"
+        :class="{ 'justify-center': !isSidebarOpen, 'justify-end': isSidebarOpen }"
+        @click="toggleSidebar"
+      >
+        <Icon v-if="isSidebarOpen" name="tabler:chevron-left" size="32" />
+        <Icon v-else name="tabler:chevron-right" size="32" />
+      </div>
+      <div class="flex flex-col">
+        <SidebarButton :show-label="isSidebarOpen" label="Locations" icon="tabler:map" href="/dashboard" />
+        <SidebarButton :show-label="isSidebarOpen" label="Add Location" icon="tabler:circle-plus-filled" href="/dashboard/add" />
+        <div class="divider" />
+        <SidebarButton :show-label="isSidebarOpen" label="Sign Out" icon="tabler:logout-2" href="/sign-out" />
+      </div>
+    </div>
+    <div class="flex-1" />
   </div>
 </template>
