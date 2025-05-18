a20af00fb01376cf85b4785a5dfa749259a9df93
Remzi Mustafaev <remarogue@gmail.com>
2025-05-18 22:02:38 +0300

    add locations list to sidebar

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
+import { useLocationStore } from '~/stores/locations';
+import { useSidebarStore } from '~/stores/sidebar';
+
 const isSidebarOpen = ref(true);
+const route = useRoute();
+const sidebarStore = useSidebarStore();
+const locationsStore = useLocationStore();
 
 onMounted(() => {
   isSidebarOpen.value = localStorage.getItem('isSidebarOpen') === 'true';
+  if (route.path !== '/dashboard') {
+    locationsStore.refresh();
+  }
 });
 
 function toggleSidebar() {
@@ -23,10 +32,39 @@ function toggleSidebar() {
         <Icon v-else name="tabler:chevron-right" size="32" />
       </div>
       <div class="flex flex-col">
-        <SidebarButton :show-label="isSidebarOpen" label="Locations" icon="tabler:map" href="/dashboard" />
-        <SidebarButton :show-label="isSidebarOpen" label="Add Location" icon="tabler:circle-plus-filled" href="/dashboard/add" />
+        <SidebarButton
+          :show-label="isSidebarOpen"
+          label="Locations"
+          icon="tabler:map"
+          href="/dashboard"
+        />
+        <SidebarButton
+          :show-label="isSidebarOpen"
+          label="Add Location"
+          icon="tabler:circle-plus-filled"
+          href="/dashboard/add"
+        />
+        <div v-if="sidebarStore.loading || sidebarStore.sidebarItems.length" class="divider" />
+        <div v-if="sidebarStore.loading" class="px-4">
+          <div class="skeleton h-4 w-full" />
+        </div>
+        <div v-if="!sidebarStore.loading && sidebarStore.sidebarItems.length" class="flex flex-col">
+          <SidebarButton
+            v-for="item in sidebarStore.sidebarItems"
+            :key="item.id"
+            :show-label="isSidebarOpen"
+            :label="item.label"
+            :icon="item.icon"
+            :href="item.href"
+          />
+        </div>
         <div class="divider" />
-        <SidebarButton :show-label="isSidebarOpen" label="Sign Out" icon="tabler:logout-2" href="/sign-out" />
+        <SidebarButton
+          :show-label="isSidebarOpen"
+          label="Sign Out"
+          icon="tabler:logout-2"
+          href="/sign-out"
+        />
       </div>
     </div>
     <div class="flex-1">
