1be7fba87e82888eaabc3a6423fc5b15362f3176
Remzi Mustafaev <remarogue@gmail.com>
2025-05-20 19:04:51 +0300

    setting up location markers, zoom map

--- components/app/map.client.vue
+++ components/app/map.client.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
 import { CENTER_CRIMEA } from '~/lib/constants';
+import { useMapStore } from '~/stores/map';
 
 const colorMode = useColorMode();
+const mapStore = useMapStore();
 const style = computed(() =>
   colorMode.value === 'dark'
     ? '/styles/dark.json'
     : 'https://tiles.openfreemap.org/styles/liberty');
 
 const zoom = 8;
+
+onMounted(() => {
+  mapStore.init();
+});
 </script>
 
 <template>
@@ -17,5 +23,16 @@ const zoom = 8;
     :zoom="zoom"
   >
     <MglNavigationControl />
+    <MglMarker v-for="point in mapStore.mapPoints" :key="point.id" :coordinates="[point.long, point.lat]">
+      <template #marker>
+        <div class="tooltip tooltip-top" :data-tip="point.label">
+          <Icon
+            name="tabler:map-pin-filled"
+            size="24"
+            class="text-primary"
+          />
+        </div>
+      </template>
+    </MglMarker>
   </MglMap>
 </template>
