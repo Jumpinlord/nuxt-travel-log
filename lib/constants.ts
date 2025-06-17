407de81f76e5e3e8249a9ec85d815d4490fbe1fc
Remzi Mustafaev <45316748+Jumpinlord@users.noreply.github.com>
2025-06-17 12:26:52 +0300

    add view location log (#77) #39

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,6 +3,11 @@ import type { LngLatLike } from 'maplibre-gl';
 export const CENTER_CRIMEA = [34.32536413216802, 45.3980561408002] as LngLatLike;
 
 export const LOCATION_PAGES = new Set(['dashboard', 'dashboard-add']);
+export const CURRENT_LOCATION_LOG_PAGES = new Set([
+  'dashboard-location-slug-id',
+  'dashboard-location-slug-id-edit',
+  'dashboard-location-slug-id-add',
+]);
 export const CURRENT_LOCATION_PAGES = new Set([
   'dashboard-location-slug',
   'dashboard-location-slug-edit',
