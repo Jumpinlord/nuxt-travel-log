cad8e5ec47170c1ae1a59d8b73958f74c2379eb9
Remzi Mustafaev <remarogue@gmail.com>
2025-06-23 10:47:34 +0300

    upload images to minio bucket

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -93,7 +93,7 @@ effect(() => {
         },
         icon: 'tabler:arrow-left',
       }, {
-        id: 'link-edit-location-log',
+        id: 'link-view-location-log',
         label: 'View Log',
         to: {
           name: 'dashboard-location-slug-id',
@@ -114,6 +114,17 @@ effect(() => {
           },
         },
         icon: 'tabler:map-pin-cog',
+      }, {
+        id: 'link-location-log-images',
+        label: 'Manage Images',
+        to: {
+          name: 'dashboard-location-slug-id-images',
+          params: {
+            slug: route.params.slug,
+            id: route.params.id,
+          },
+        },
+        icon: 'tabler:photo-cog',
       }];
     }
   }
